1d9d4c63481995c2508bdd41186de3c5a1068a22
sukyumKim <ikks06luck@naver.com>
2014-10-23 10:20:14 -0700

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
    config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'polar-mesa-2125.herokuapp.com'
+  host = 'fierce-atoll-3696.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
