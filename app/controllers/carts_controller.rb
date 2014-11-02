62ed0619eaef3e6cc656e0c269a8b8ee9b1d694a
gyotaek <leegyotaek@gmail.com>
2014-11-03 01:37:29 +0900

    cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -32,7 +32,8 @@ class CartsController < ApplicationController
     
    end
 
-   @cart_item = current_user.cart_items.find_by(item_id: id)
+   @cart_item = CartItem.where(owner_id = current_user.id , item_id = @product.id)
+   #current_user.cart_items.find_by(item_id: id)
 
    respond_to do |format|
       format.html { redirect_to cart_path}
