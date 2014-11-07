e3002c0de218531d341b71046a3b32249c24981f
gyotaek <leegyotaek@gmail.com>
2014-11-08 00:59:21 +0900

    ???

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
      if logged_in?
-  	   @micropost = current_user.microposts.build 
-     @user = current_user
-     @microposts = current_user.microposts.paginate(page: params[:page])
+      @user ||= current_user
+  	   @micropost = @user.microposts.build 
+     
+     @microposts = @user.microposts.paginate(page: params[:page])
      @comments = @micropost.comments.all
      @comment = @micropost.comments.build
-     @mailbox ||= current_user.mailbox
+     
 
      end
   end
