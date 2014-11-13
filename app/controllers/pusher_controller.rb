841253ddb360e0685600da2fd2719b44b78e31d0
gyotaek <leegyotaek@gmail.com>
2014-11-13 10:57:32 +0900

    presence

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -19,9 +19,28 @@ Pusher["mychannel-#{current_user.id}"].trigger("my-event", {:type => "new_purcha
 render :text => "test pusher"
 end
 
-def auth
-response = Pusher[params[:channel_name]].authenticate(params[:socket_id])
-render :json => response
-end
+# def auth
+# response = Pusher[params[:channel_name]].authenticate(params[:socket_id])
+# render :json => response
+# end
 
+
+
+
+def auth
+    if current_user
+      response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {
+        :user_id => current_user.id, # => required
+        :user_info => { # => optional - for example
+          :name => current_user.name,
+          :email => current_user.email
+        }
+      })
+      render :json => response
+    else
+      render :text => "Forbidden", :status => '403'
+    end
+  end
+
+  
 end
