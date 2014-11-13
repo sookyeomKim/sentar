1f1789739ccd09192c0f77bc6e6b2a662e35413e
gyotaek <leegyotaek@gmail.com>
2014-11-14 08:52:19 +0900

    pusher

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,4 +1,3 @@
-# require 'pusher'
-
-# Pusher.url = "http://37d5027632186dcd7679:0db9ca86ee7b48b91596@api.pusherapp.com/apps/96196"
+require 'pusher'
+Pusher.url = "http://a49249ec2328f5c4474d:247615e156ccedebb1f2@api.pusherapp.com/apps/96416"
 
